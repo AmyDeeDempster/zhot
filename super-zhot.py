fa2876b3e923817a80dd53168349ce7858fd5cde
Amy Dee Dempster <amy.dempster@student.rmit.edu.au>
2018-07-31 22:44:18 +1000

    Tweaks as per Puppy

--- super-zhot.py
+++ super-zhot.py
@@ -14,38 +14,42 @@ def main():
 	moves = []
 	with open(move_fn) as move_fh:
 		rows = csv.reader(move_fh, delimiter=',', quotechar='"')
-		for row in rows:
-			# Ignore blank lines.
-			if len(row):
-				moves.append(row)
+		# Ignore blank lines.
+		moves = [row for row in rows if len(row)]
 
 	# Turn list of strings into list of Move objects
 	move_names = [item[0] for item in moves]
 	for i, move_info in enumerate(moves):
 		new = Move(i, len(moves), move_info, move_names)
 		move_objs.append(new)
+	move_names_str = ", ".join(move_names)
 
-	print("Rules of the game:")
-	for move in move_objs:
-		for loser, verb in move.beats.items():
-			print(" ".join((move.move, verb, loser)) + ".")
-
+	
 	# Play the game
 	while True:
-		human = input().strip()
-		if not human:
+		print("Options: %s." % move_names_str )
+		print("What is your move? ", end="")
+
+		# Get human player's move.
+		stdin = input().strip()
+
+		if not stdin or stdin in ("exit", "quit"):
 			print("Exiting.")
 			break
-		ai = random.choice(move_objs)
+		elif stdin in ("help", "moves", "?"):
+			rules(move_objs)
 
 		for candidate in move_objs:
-			if human.casefold() in candidate.move.casefold():
+			if stdin.casefold() in candidate.move.casefold():
 				human = candidate
 				break
 		else:
 			continue
+
+		# Get computer's move.
+		ai = random.choice(move_objs)
 		
-		print("You play " + human.move + " and I play " + ai.move + ".")
+		print("You play", human.move, "and I play", ai.move, end=". ")
 		if human == ai:
 			print("Stalemate.")
 		elif human.move in ai.beats:
@@ -54,6 +58,12 @@ def main():
 			print(human.result_vs(ai.move))
 		else:
 			print("This code should not be reached.")
+
+def rules(move_objs):
+	print("Rules of the game:")
+	for move in move_objs:
+		for loser, verb in move.beats.items():
+			print(" ".join((move.move, verb, loser)) + ".")
 
 class Move:
 	def __init__(self, number, total, info, move_names):
