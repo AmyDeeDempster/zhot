4b56f3435193dfd84204f8ec6031abdc31c8f278
Amy Dee Dempster <amy.dempster@student.rmit.edu.au>
2018-08-01 01:33:28 +1000

    More OOP

--- super-zhot.py
+++ super-zhot.py
@@ -5,65 +5,50 @@ import random
 move_fn = 'moves.csv'
 generic_verb = 'beats'
 
-
 def main():
-	move_objs = []
-	row_num = 0
-
-	# Turn CSV into list of strings.
-	moves = []
-	with open(move_fn) as move_fh:
-		rows = csv.reader(move_fh, delimiter=',', quotechar='"')
-		# Ignore blank lines.
-		moves = [row for row in rows if len(row)]
-
-	# Turn list of strings into list of Move objects
-	move_names = [item[0] for item in moves]
-	for i, move_info in enumerate(moves):
-		new = Move(i, len(moves), move_info, move_names)
-		move_objs.append(new)
-	move_names_str = ", ".join(move_names)
-
+	game = Game()
 	
 	# Play the game
 	while True:
-		print("Options: %s." % move_names_str )
+		print("Options: %s." % game.move_names_str )
 		print("What is your move? ", end="")
 
-		# Get human player's move.
-		stdin = input().strip()
-
-		if not stdin or stdin in ("exit", "quit"):
-			print("Exiting.")
-			break
-		elif stdin in ("help", "moves", "?"):
-			rules(move_objs)
-
-		for candidate in move_objs:
-			if stdin.casefold() in candidate.move.casefold():
-				human = candidate
-				break
-		else:
-			continue
-
-		# Get computer's move.
-		ai = random.choice(move_objs)
+		human = get_human_move(game)
+		ai    = random.choice(game.move_objs)
 		
-		print("You play", human.move, "and I play", ai.move, end=". ")
-		if human == ai:
-			print("Stalemate.")
-		elif human.move in ai.beats:
-			print(ai.result_vs(human.move))
-		elif ai.move in human.beats:
-			print(human.result_vs(ai.move))
-		else:
-			print("This code should not be reached.")
+		# Check whether that was a real move, or perhaps a move to quit.
+		if human.move:
+			print("You play", human.move, "and I play", ai.move, end=". ")
+			print(round_results(human, ai))
+		elif human.quitting:
+			break
+	print("Exiting.")
 
-def rules(move_objs):
-	print("Rules of the game:")
-	for move in move_objs:
-		for loser, verb in move.beats.items():
-			print(" ".join((move.move, verb, loser)) + ".")
+def get_human_move(game):
+	stdin = input().strip()
+
+	if not stdin or stdin in ("exit", "quit"):
+		return Admin_move(True)
+	elif stdin in ("help", "moves", "?"):
+		print(game.rules)
+		return Admin_move(False)
+
+	for candidate in game.move_objs:
+		if stdin.casefold() in candidate.move.casefold():
+			return candidate
+	else:
+		return Admin_move(False)
+
+def round_results(human, ai):
+	if human == ai:
+		return "Stalemate."
+	elif human.move in ai.beats:
+		return ai.result_vs(human.move)
+	elif ai.move in human.beats:
+		return human.result_vs(ai.move)
+	else:
+		raise Exception("Invalid move")
+
 
 class Move:
 	def __init__(self, number, total, info, move_names):
@@ -82,5 +67,32 @@ class Move:
 			self.beats[loser] = verb
 	def result_vs(self, enemy_move):
 		return " ".join((self.move, self.beats[enemy_move], enemy_move)) + "."
+
+class Admin_move:
+	def __init__(self, quitting):
+		self.move = False
+		self.quitting = quitting
+
+class Game:
+	def __init__(self):
+		# Turn CSV into list of strings.
+		with open(move_fn) as move_fh:
+			rows = csv.reader(move_fh, delimiter=',', quotechar='"')
+			# Ignore blank lines.
+			moves = [row for row in rows if len(row)]
+
+		# Turn list of strings into list of Move objects
+		self.move_names = [item[0] for item in moves]
+		self.move_names_str = ", ".join(self.move_names)
+		self.move_objs = []
+		for i, move_info in enumerate(moves):
+			new = Move(i, len(moves), move_info, self.move_names)
+			self.move_objs.append(new)
+
+		# Build a string with game rules.
+		self.rules = "Rules of the game:\n"
+		for obj in self.move_objs:
+			for loser, verb in obj.beats.items():
+				self.rules += (" ".join((obj.move, verb, loser)) + ".\n")
 
 main()
