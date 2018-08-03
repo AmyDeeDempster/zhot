6d91bc2df99270616543a0ee934bfa03d57f9e5e
Amy Dee DEMPSTER <amy.dempster@student.rmit.edu.au>
2018-08-04 00:23:25 +1000

    Finished game (various OOP improvements, and game messages)

--- super-zhot.py
+++ super-zhot.py
@@ -4,21 +4,38 @@ import random
 import sys
 
 def main():
-	try:
-		game = Game(sys.argv[1])
-	except:
-		print("Provide a CSV file of possible moves, as a command-line argument.")
-		exit()
+	"An extension to the classic game of Scissors-Paper-Stone, Roshambo, or Zhot."
+	game = Game(sys.argv[1])
 
 	# Play the game
 	while True:
-		round = Round(game)
-		print(round.moves, end="")
-		print(round.outcome)
-		game.score['human'] += round.score['human']
-		game.score['ai']    += round.score['ai'] 
+		the_round = Round(game)
+		print(the_round.moves + the_round.outcome)
+		game.score.human += the_round.score.human
+		game.score.ai    += the_round.score.ai
 
-	
+class Score:
+	"Essentially just a dictionary, but with a custom representation."
+	def __init__(self, human=0, ai=0):
+		self.human = human
+		self.ai    = ai
+	def upshot(self):
+		if self.ai > self.human:
+			return "I win."
+		if self.human > self.ai:
+			return "You win."
+		else:
+			return "We have tied."
+	def dict(self):
+		"Dictionary representation of this class"
+		return dict(human=self.human, ai=self.ai)
+	def __repr__(self):
+		return str(self.dict())
+	def __str__(self):
+		return "You have scored {} and I have scored {}.".format(
+			self.human,
+			self.ai
+		)
 
 class Round:
 	"""Object that deals with a single round of the game""" 
@@ -26,47 +43,64 @@ class Round:
 		print("Options: %s." % game.move_names )
 		print("What is your move? ", end="")
 
-		# These return instances of Move() or Admin_move()
+		# These return instances of Move() or AdminMove()
 		human = self.get_human_move(game)
 		ai    = random.choice(game.move_objs)
 		
 		# Check whether that was a real move, or perhaps a move to quit.
 		self.moves = str()
 		self.outcome = str()
-		self.score = dict(human=0, ai=0)
+		self.score = Score()
 		if human.move:
 			self.moves += "You play " + human.move + " and I play " + ai.move + ". "
 			if human == ai:
 				self.outcome = "Stalemate."
 			elif human.move in ai.beats:
 				self.outcome = ai.result_vs(human.move)
-				self.score['ai'] = 1
+				self.outcome += " " + random.choice(Move.remarks['victorious'])
+				self.score.ai = 1
 			elif ai.move in human.beats:
 				self.outcome = human.result_vs(ai.move)
-				self.score['human'] = 1
+				self.outcome += " " + random.choice(Move.remarks['conceding'])
+				self.score.human = 1
 			else:
 				raise Exception("Invalid move")
+			self.outcome += "\n"
+			game.rounds += 1
 		elif human.quitting:
-			print("Exiting.")
-			print(game.score)
+			print("Exiting game.")
+			print("{} {} rounds played.".format(game.score, game.rounds))
+			print(game.score.upshot())
 			exit()
 
 	def get_human_move(self, game):
+		"Parse player input."
 		stdin = input().strip()
 		if not stdin or stdin in ("exit", "quit"):
-			return Admin_move(True)
+			return AdminMove(quitting=True)
+		elif stdin in ("score", "points"):
+			print(game.score)
+			return AdminMove(quitting=False)
+		elif stdin in ("rounds", "length"):
+			print("{} rounds have been played so far.".format(game.rounds))
+			return AdminMove(quitting=False)
 		elif stdin in ("help", "moves", "rules", "?"):
 			print(game.rules)
-			return Admin_move(False)
+			return AdminMove(quitting=False)
 		for candidate in game.move_objs:
 			if stdin.casefold() in candidate.move.casefold():
 				return candidate
 		else:
-			return Admin_move(False)
+			return AdminMove(quitting=False)
 
 class Move:
 	"""Object that contains the name of a move,
 	   the moves it defeats, and how it defeats them."""
+	remarks = dict(
+		victorious=("Heheh.", "The round is mine!", "I win.", "You loser."),
+		conceding=("OK.", "Drat!", "You’ve won that one.", "Dammit.", "Argh!")
+	)
+
 	generic_verb = 'beats'
 	def __init__(self, number, total, info, move_names):
 		self.move = info.pop(0)
@@ -77,7 +111,7 @@ class Move:
 			try:
 				verb = info.pop(0).strip()
 			except:
-				verb = generic_verb
+				verb = self.generic_verb
 			# Get a string like "Paper".
 			loser = move_names[(number + offset) % total]
 			# Set a dictionary entry like {"Paper": "cuts"}
@@ -85,19 +119,41 @@ class Move:
 	def result_vs(self, enemy_move):
 		return " ".join((self.move, self.beats[enemy_move], enemy_move)) + "."
 
-class Admin_move:
+class AdminMove(Move):
 	"Like Move(), but not concerned with actually playing, but rather stuff like displaying help."
-	def __init__(self, quitting):
+	def __init__(self, quitting=False):
 		self.move = False
 		self.quitting = quitting
 
-class Game:
+class NotOddError(ValueError):
+	pass
+
+class InsufficientMovesError(ValueError):
+	pass
+
+class Game():
+	"Object that contains a list of Move objects and strings with info on the game."
 	def __init__(self, filename):
 		# Turn CSV into list of strings.
-		with open(filename) as filehandle:
-			rows = csv.reader(filehandle, delimiter=',', quotechar='"')
-			# Ignore blank lines.
-			moves = [row for row in rows if len(row)]
+		try:
+			with open(filename) as filehandle:
+				rows = csv.reader(filehandle, delimiter=',', quotechar='"')
+				# Ignore blank lines.
+				moves = [row for row in rows if len(row)]
+				if len(moves) % 2 == 0:
+					raise NotOddError("For a fair game, there must be an odd number of moves.")
+				if len(moves) < 3:
+					raise InsufficientMovesError("Multiple moves are necessary.")
+		except UnicodeDecodeError as error:
+			print("That is not a valid CSV file.")
+			exit()
+		except IsADirectoryError:
+			print("That’s not a CSV file.  It’s a directory.")
+			exit()
+		except (FileNotFoundError, NotOddError, InsufficientMovesError) as error:
+			print("There has been a problem opening your CSV file.")
+			print(error)
+			exit()
 
 		# Turn list of strings into list of Move objects
 		move_names = [item[0] for item in moves]
@@ -107,15 +163,17 @@ class Game:
 			self.move_objs.append(new)
 
 		# Keep score
-		self.score = dict(human=0, ai=0)
+		self.score = Score()
+		self.rounds = 0
 
 		# Outside of this class, we'll need this list of moves stringified.
 		self.move_names = ", ".join(move_names)
 
 		# Build a string with game rules.
-		self.rules = "Rules of the game:\n"
+		self.rules = "\nRules of the game:\n"
 		for obj in self.move_objs:
 			for loser, verb in obj.beats.items():
 				self.rules += (" ".join((obj.move, verb, loser)) + ".\n")
+		self.rules += "\nMake one of these moves, or use ‘score’, ‘rounds’, ‘help’ or ‘exit’."
 
 main()
