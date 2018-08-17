8e8a67cc3597de99e87af1f46a15fe936bb0be51
Amy Dee Dempster <amy.dempster@student.rmit.edu.au>
2018-08-18 02:36:02 +1000

    Fixed lint errors

--- setup.py
+++ setup.py
@@ -7,24 +7,22 @@ from setuptools import setup
 
 
 version = re.search(
-	'^__version__\s*=\s*"(.*)"',
+	r'^__version__\s*=\s*"(.*)"',
 	open('zhot/zhot.py').read(),
 	re.M
-	).group(1)
+).group(1)
 
 with open("README.rst", "rb") as f:
 	long_descr = f.read().decode("utf-8")
 
 setup(
-	name = "cmdline-zhot",
+	name = "cli-zhot",
 	packages = ["zhot"],
-	entry_points = {
-		"console_scripts": ['zhot = zhot.zhot:main']
-		},
+	entry_points = {"console_scripts": ['zhot = zhot.zhot:main']},
 	version = version,
-	description = "Python command line application bare bones template.",
+	description = "Command-line implementation of Zhot, an extensible Scissors-Paper-Stone game.",
 	long_description = long_descr,
 	author = "Amy Dee DEMPSTER",
 	author_email = "amy.dempster@student.rmit.edu.au",
 	url = "",
-	)
+)
