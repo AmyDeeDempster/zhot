b93ac3a18dbc85b8598d69dd243dff210046021f
Amy Dee Dempster <a.d.dempster@gmail.com>
2018-08-20 13:55:12 +1000

    Look in different dirs

--- zhot/__init__.py
+++ zhot/__init__.py
@@ -2,12 +2,21 @@
 """zhot.__init__.py: provides means of getting version number."""
 
 import re
-from os import listdir
+from os import (listdir, chdir)
 
 def get_version():
 	pattern = r'version-([\d\.]+)'
 	try:
-		fn = [fn for fn in listdir() if 'README' in fn][0]
+		print("This is the current dir:")
+		print(listdir())
+		print("This is up one:")
+		print(listdir('..'))
+
+		try:
+			fn = [fn for fn in listdir() if 'README' in fn][0]
+		except:
+			chdir('..')
+			fn = [fn for fn in listdir() if 'README' in fn][0]
 		with open(fn, "rb") as fh:
 			README_STR = fh.read().decode('utf-8')
 		return re.search(pattern, README_STR, re.M).group(1)
